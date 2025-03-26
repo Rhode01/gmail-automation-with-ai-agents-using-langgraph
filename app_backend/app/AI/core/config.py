80d9da85594dc2d59dfd8dc490d6c12bcc78e252
Rhode01 <Rhodrickmhone@gmail.com>
2025-03-26 16:55:17 +0200

    .

--- app_backend/app/AI/core/config.py
+++ app_backend/app/AI/core/config.py
@@ -1,11 +1,17 @@
 from langchain_openai import ChatOpenAI
-from openai import OpenAI
 class AIModel:
     def __init__(self):
-        self.model = None
-        self.init_model() 
-
+        self.model = self.init_model() 
     def init_model(self):
-        self.model = ChatOpenAI(temperature=0 ,model="gpt-4-turbo")
+        return ChatOpenAI(
+            temperature=0,
+            model="gpt-4-turbo",
+            max_tokens=600, 
+            request_timeout=30,
+            max_retries=3,
+            client=None,
+            http_client=None,
+            verbose=False
+        )
 
-client= AIModel()
+client= AIModel().model()
